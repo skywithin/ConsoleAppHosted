fcede2f129a2e501dec5809e6b4f8503a78a7f96
Max Vagner <sydneymax@gmail.com>
2021-06-23 15:37:59 +1000

    Initial commit

---
+++ ConsoleAppHosted/GreetingService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppHosted
+{
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> _logger;
+        private readonly IConfiguration _config;
+
+        public GreetingService(
+            ILogger<GreetingService> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _config = configuration;
+        }
+
+        public void Run()
+        {
+            var loopTimes = _config.GetValue<int>("LoopTimes");
+
+            for (int i = 0; i < loopTimes; i++)
+            {
+                _logger.LogInformation("Run number {runNumber}", i);
+            }
+        }
+    }
+
+    public interface IGreetingService
+    {
+        void Run();
+    }
+}
---
+++ ConsoleAppHosted/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+namespace ConsoleAppHosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder();
+
+            PrimeConfigBuilder(configBuilder);
+
+            var config = configBuilder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application starting");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            svc.Run();
+        }
+
+        static void PrimeConfigBuilder(IConfigurationBuilder configBuilder)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env ?? "Development"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+
+    }
+}
